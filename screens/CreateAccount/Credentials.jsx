2ab9b99f28c4f315c0f904d87c5bfd7822a18a97
juliagbmaciel <jpohje@gmail.com>
2023-11-08 11:30:06 -0300

    Missing to fix the create legal account

--- screens/CreateAccount/Credentials.jsx
+++ screens/CreateAccount/Credentials.jsx
@@ -1,22 +1,22 @@
 import { Image, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
 import styles from './createAccount.style'
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
 import { Formik } from 'formik'
 import { Ionicons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import { createClient, createUser, getToken, createPhysicalClient, createAccount } from '../../src/services/api'
+import { createClient, createUser, getToken, createPhysicalClient, createAccount, createLegalClient } from '../../src/services/api'
 import { setToken, setSigned, setClientData, setAccountData } from '../../src/reducers/actions'
 
 
 
 const Credentials = ({ navigation }) => {
 
-  const { 
-    completeName, socialName, 
-    dateOfBirth, accountType, rg, cpf, 
-    cnpj, inscEstadual, inscMunicipal, signed, token  
+  const {
+    completeName, socialName,
+    dateOfBirth, accountType, rg, cpf,
+    cnpj, inscEstadual, inscMunicipal, signed, token
   } = useSelector(state => {
     return state.userReducer
   })
@@ -24,6 +24,7 @@ const Credentials = ({ navigation }) => {
 
 
   const [obscureText, setObscureText] = useState(true)
+  const [tokenData, setTokenData] = useState('')
   const dispatch = useDispatch()
 
   const validationSchema = Yup.object().shape({
@@ -33,121 +34,139 @@ const Credentials = ({ navigation }) => {
   })
 
   const handleSignIn = async (values) => {
-    
-      try {
+
+    try {
+      if (accountType === 'Pessoa Física') {
         const user = await createUser(cpf, values.password, completeName);
+
         console.log("Usuário criado:", user);
-      
-        const tokenData = await getToken(cpf, values.password);
+
+        const tokenDataApi = await getToken(cpf, values.password);
+
+        setTokenData(tokenDataApi.auth_token)
+        dispatch(setToken(tokenDataApi.auth_token));
+
+      } else {
+        const user = await createUser(cnpj, values.password, completeName);
+
+        console.log("Usuário criado:", user);
+
+        const tokenDataApi = await getToken(cnpj, values.password);
+
+        console.log('token lixo: ', tokenDataApi.auth_token)
+        setTokenData(tokenDataApi.auth_token)
+
         dispatch(setToken(tokenData.auth_token));
-        console.log("Token obtido:", token);
-      
-        console.log("Dados:", completeName, socialName, dateOfBirth);
-      
-        const client = await createClient(completeName, socialName, dateOfBirth, tokenData.auth_token);
-        console.log("Cliente criado:", client);
-
-        const account = await createAccount(tokenData.auth_token)
-        console.log("Conta criada ", account)
-        dispatch(setAccountData({account}))
-
-        if(accountType === 'Pessoa Física'){
-          const physicalClient = await createPhysicalClient(rg, tokenData.auth_token)
-          dispatch(setClientData({physicalClient}))
-          
-          dispatch(setSigned(true))
-        }else{
-
-        }
-
-      } catch (error) {
-        console.log(error)
-        //  Alert.alert(
-        //         'CPF informado já possui uma conta',
-        //         'Este usuário já existe, faça login nesta conta',
-        //         [
-        //           { text: 'Login', style: 'cancel', onPress: () => {} },
-        //           {
-        //             text: 'Cancelar',
-        //             style: 'destructive',
-        //             onPress: () => {
-                        
-        //             },
-        //           },
-        //         ]
-        //       );
       }
+
+
+      const client = await createClient(completeName, socialName, dateOfBirth, tokenData);
+      console.log("Cliente criado:", client);
+
+      const account = await createAccount(tokenData.auth_token)
+      console.log("Conta criada ", account)
+      dispatch(setAccountData({ account }))
+
+      if (accountType === 'Pessoa Física') {
+        const physicalClient = await createPhysicalClient(rg, tokenData.auth_token)
+        dispatch(setClientData({ physicalClient }))
+
+        dispatch(setSigned(true))
+      } else {
+        const legalClient = await createLegalClient(tokenData.auth_token, cnpj, inscEstadual, inscMunicipal )
+        dispatch(setClientData({ legalClient }))
+
+        dispatch(setSigned(true))
+      }
+
+    } catch (error) {
+      console.log(error)
+      //  Alert.alert(
+      //         'CPF informado já possui uma conta',
+      //         'Este usuário já existe, faça login nesta conta',
+      //         [
+      //           { text: 'Login', style: 'cancel', onPress: () => {} },
+      //           {
+      //             text: 'Cancelar',
+      //             style: 'destructive',
+      //             onPress: () => {
+
+      //             },
+      //           },
+      //         ]
+      //       );
+    }
   }
 
 
   return (
     <TouchableWithoutFeedback onPressIn={() => Keyboard.dismiss()}>
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <Image
-          source={require('../../assets/img/logo-bigger.png')}
-        />
-      </View>
-      <View style={styles.mainContent}>
-        <Text style={styles.title}>Criar conta</Text>
-        <Formik
-          initialValues={{ password: '' }}
-          validationSchema={validationSchema}
-          onSubmit={(values) => navigation.navigate('FirstWelcome')}
-        >
-          {({ handleChange, handleBlur, handleSubmit, touched, errors, isValid, setFieldTouched, values }) => (
-            <View style={styles.logContent}>
-              { values.password == '' ? isValid = false : isValid = isValid}
-              <View>
-              </View>
-              <View>
-                <Text style={styles.label}>
-                  Senha
-                </Text>
-                <View style={styles.inputPassword}>
-                  <TextInput
-                    secureTextEntry={obscureText}
-                    keyboardType={'default'}
-                    onChangeText={handleChange('password')}
-                    onFocus={() => { setFieldTouched('password') }}
-                    onBlur={() => { setFieldTouched('password', '') }}
-                    value={values.password}
-                    color={"#fff"} 
-                    maxLength={15}
-                    style={{ height: "100%", width: "85%" }}
-                  />
-                  <TouchableOpacity onPressIn={() => setObscureText(!obscureText)}>
-                    <Ionicons name={obscureText ? 'eye-off-outline' : 'eye-outline'} size={20} color={"#ccc"} />
-                  </TouchableOpacity>
-
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image
+            source={require('../../assets/img/logo-bigger.png')}
+          />
+        </View>
+        <View style={styles.mainContent}>
+          <Text style={styles.title}>Criar conta</Text>
+          <Formik
+            initialValues={{ password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values) => navigation.navigate('FirstWelcome')}
+          >
+            {({ handleChange, handleBlur, handleSubmit, touched, errors, isValid, setFieldTouched, values }) => (
+              <View style={styles.logContent}>
+                {values.password == '' ? isValid = false : isValid = isValid}
+                <View>
                 </View>
-                {touched.password && errors.password && (
-                  <Text style={{ fontFamily: 'semibold', color: "red" }}>{errors.password}</Text>
-                )}
+                <View>
+                  <Text style={styles.label}>
+                    Senha
+                  </Text>
+                  <View style={styles.inputPassword}>
+                    <TextInput
+                      secureTextEntry={obscureText}
+                      keyboardType={'default'}
+                      onChangeText={handleChange('password')}
+                      onFocus={() => { setFieldTouched('password') }}
+                      onBlur={() => { setFieldTouched('password', '') }}
+                      value={values.password}
+                      color={"#fff"}
+                      maxLength={15}
+                      style={{ height: "100%", width: "85%" }}
+                    />
+                    <TouchableOpacity onPressIn={() => setObscureText(!obscureText)}>
+                      <Ionicons name={obscureText ? 'eye-off-outline' : 'eye-outline'} size={20} color={"#ccc"} />
+                    </TouchableOpacity>
+
+                  </View>
+                  {touched.password && errors.password && (
+                    <Text style={{ fontFamily: 'semibold', color: "red" }}>{errors.password}</Text>
+                  )}
+                </View>
+                <View style={Platform.OS === "ios" ? styles.buttonsView : styles.buttonsViewAnd} >
+
+
+                  <TouchableOpacity style={styles.buttonPrimary} onPress={() => {
+                    navigation.goBack()
+                  }}>
+                    <Ionicons name='arrow-back' size={20} />
+                    <Text style={{ fontFamily: 'regular' }}>Voltar</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPressIn={isValid ? () => handleSignIn(values) : () => { }} style={isValid ? styles.buttonPrimary : styles.buttonInactive} onPress={() => handleSignIn}>
+                    <Text style={isValid ? { fontFamily: 'regular' } : { color: "#313131" }}>Próximo</Text>
+                    <Ionicons style={!isValid && { color: "#313131" }} name='arrow-forward' size={20} />
+                  </TouchableOpacity>
+                </View>
+
+
+
               </View>
-              <View style={Platform.OS === "ios" ? styles.buttonsView : styles.buttonsViewAnd} >
+            )}
+          </Formik>
+        </View>
 
-
-                <TouchableOpacity style={styles.buttonPrimary} onPress={() => {
-                  navigation.goBack()
-                }}>
-                  <Ionicons name='arrow-back' size={20} />
-                  <Text style={{ fontFamily: 'regular' }}>Voltar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPressIn={isValid ? () => handleSignIn(values) : () => { }} style={isValid ? styles.buttonPrimary : styles.buttonInactive} onPress={() => handleSignIn}>
-                  <Text style={isValid ? { fontFamily: 'regular' } : { color: "#313131" }}>Próximo</Text>
-                  <Ionicons style={!isValid && { color: "#313131" }} name='arrow-forward' size={20} />
-                </TouchableOpacity>
-              </View>
-
-
-
-            </View>
-          )}
-        </Formik>
       </View>
-
-    </View>
     </TouchableWithoutFeedback>
   )
 }
