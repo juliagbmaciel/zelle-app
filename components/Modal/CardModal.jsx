d5319139be5530d424154832e7944abdbdcd638d
juliagbmaciel <jpohje@gmail.com>
2023-11-10 11:26:34 -0300

    Creating Modal Component

---
+++ components/Modal/CardModal.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, Modal } from 'react-native'
+import React from 'react'
+import styles from './cardModalStyle'
+
+
+const CardModal = ({ }) => {
+    return (
+        <View>
+            <Modal
+                animationType='slide'
+            >
+
+            </Modal>
+        </View>
+    )
+}
+
+export default CardModal
+
