a2748930ce47746c2766e45201aa061e5f2a0cee
juliagbmaciel <jpohje@gmail.com>
2023-11-13 11:31:02 -0300

    "Putting a new conditional block to card banner"

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -2,27 +2,37 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import style from './style'
 import mastercardLogo from '../../assets/img/mastercard_logo.png'
+import visaLogo from '../../assets/img/visa_logo.png'
+import PressableButton from '../Buttons'
 
 
 
 const Card = ({props}) => {
-    console.log("propssssssssss: ", props.item.limit)
     const number = props.item.number;
+    
     const limit = parseFloat(props.item.limit);
     const limitFormatted = limit.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+    const banner = props.item.banner
+
+    const objectDate = new Date(props.item.expiration);
+    const newDate = `${objectDate.getMonth() + 1}/${objectDate.getFullYear().toString().slice(-2)}`;
+
     return (
         <View style={style.cardContainer}>
             <View style={style.blobOne} />
             <View style={style.blobTwo} />
-            <Image source={mastercardLogo} style={style.logo} />
+            <Image source={banner === 'Visa' ? visaLogo : mastercardLogo} style={style.logo} />
             <View style={style.content}>
                 <View>
                     <Text style={style.label}>Limite Total</Text>
                     <Text style={style.balance}>{limitFormatted}</Text>
+                    <PressableButton  bgColor={'#000'} radius={'10px'} title={"Ver fatura"} colorTitle={'#D3FE57'} />
                 </View>
+                
                 <View style={style.flexBetween}>
                     <Text style={style.labelN}>{number}</Text>
-                    <Text style={style.labelN}>9/25</Text>
+                    <Text style={style.labelN}>{newDate}</Text>
                 </View>
             </View>
         </View>
