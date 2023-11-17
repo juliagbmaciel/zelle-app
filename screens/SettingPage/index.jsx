b311321067b080393c0241ec21b83796fed04ed2
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-17 11:27:25 -0300

    Creating two new settings pages

--- screens/SettingPage/index.jsx
+++ screens/SettingPage/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, Alert } from 'react-native'
+import { View, Text, Image, Alert, ScrollView } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import defaultStyle from '../../src/defaultStyle/style'
@@ -6,9 +6,13 @@ import { useSelector } from 'react-redux'
 import Input from '../../components/Input'
 import PressableButton from '../../components/Buttons'
 import { updateClient } from '../../src/services/api'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { CommonActions } from '@react-navigation/native'
+import { getContacts, createContact, sendPicture } from '../../src/services/api'
+import { Ionicons } from '@expo/vector-icons'
+import * as ImagePicker from 'expo-image-picker';
+
 
 
 export default function SettingPage({ route }) {
@@ -17,6 +21,21 @@ export default function SettingPage({ route }) {
         return state.userReducer
     })
     const [nameInput, setNameInput] = useState('')
+    const [emailInput, setEmailInput] = useState('')
+    const [numberInput, setNumberInput] = useState(0)
+
+    const [image, setImage] = useState('')
+
+
+
+
+
+    const [contactData, setContactData] = useState({
+        data: null,
+        loading: true,
+        error: null,
+    })
+
     const navigation = useNavigation()
 
 
@@ -29,42 +48,140 @@ export default function SettingPage({ route }) {
         }
     };
 
+    const saveContact = async () => {
+        try {
+            const contact = await createContact(token, numberInput, emailInput);
+            console.log("Contact: ", contact);
+        } catch (error) {
+            console.error("Erro ao criar contato aqui", error);
+        }
+    };
+
     const willUpdateFeedback = (field, value) => {
-        if (value.length < 10){
+        if (value.length < 10) {
             Alert.alert(
                 '',
                 'Nome não pode ser menor que 10 caracteres',
                 [
-                  {
-                    text: 'OK',
-                    style: 'destructive',
-                    onPress: () => {},
-                  },
+                    {
+                        text: 'OK',
+                        style: 'destructive',
+                        onPress: () => { },
+                    },
                 ]
-              );
+            );
             return
         }
         updateField(field, value)
+        createAlert('Nome alterado com sucesso')
+    }
+
+    const willSaveContact = () => {
+        if (numberInput.length < 11 || emailInput.length < 12) {
+            Alert.alert(
+                '',
+                'Os valores não podem ser menores que 12, e nem vazios',
+                [
+                    {
+                        text: 'OK',
+                        style: 'destructive',
+                        onPress: () => { },
+                    },
+                ]
+            );
+            return
+        }
+        saveContact(nameInput, emailInput)
+        createAlert('Contato adicionado com sucesso!')
+    }
+
+    const createAlert = (msg) => {
         Alert.alert(
             '',
-            'Nome alterado com sucesso!',
+            `${msg}`,
             [
-              {
-                text: 'OK',
-                style: 'destructive',
-                onPress: () => {
-                    navigation.dispatch(CommonActions.reset({
-                        index: 0,
-                        routes: [
-                            { name: 'Bottom Navigation' },
-                        ],
-                    }))
-    
+                {
+                    text: 'OK',
+                    style: 'destructive',
+                    onPress: () => {
+                        navigation.dispatch(CommonActions.reset({
+                            index: 0,
+                            routes: [
+                                { name: 'Bottom Navigation' },
+                            ],
+                        }))
+
+                    },
                 },
-              },
             ]
-          );
+        );
     }
+
+    const fetchDataContact = async () => {
+        setContactData((prevState) => ({ ...prevState, loading: true }));
+
+        try {
+            const contacts = await getContacts(token);
+            setContactData({
+                data: contacts,
+                loading: false,
+                error: null,
+            });
+        } catch (error) {
+            setContactData({
+                data: null,
+                loading: false,
+                error: "Erro ao buscar contatos",
+            });
+        }
+    };
+
+
+
+    useEffect(() => {
+        if (title === 'Contato') {
+            fetchDataContact();
+        }
+    }, []);
+
+
+    const gallery = async () => {
+        console.log('uaiii')
+        const result = ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+        })
+
+        console.log((await result))
+
+        if (!(await result).canceled) {
+            setImage((await result).assets[0].uri)
+        }else{
+            return
+        }
+    }
+
+    const sendImageFunc = async () => {
+        console.log(image)
+        const formData = new FormData();
+    
+        formData.append('picture', {
+            uri: image,
+            name: 'nome_da_imagem.jpg',
+            type: 'image/jpeg',
+        });
+    
+        try {
+            console.log(formData)
+            const response = await sendPicture(token, formData)
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erro ao criar instância do modelo comm', error);
+        }
+    };
+    
 
 
     return (
@@ -82,22 +199,58 @@ export default function SettingPage({ route }) {
                         <Text style={styles.labelN}>{accountData.client.client.name}</Text>
                     </View>
 
-                    <Input label={'Novo nome'} onChange={(value, la) => setNameInput(value)}/>
-                    <PressableButton bgColor={'#D3FE57'} title={'Confirmar'} onPress={() => {willUpdateFeedback('name', nameInput)}}/>
+                    <Input label={'Novo nome'} onChange={(value, la) => setNameInput(value)} />
+                    <PressableButton bgColor={'#D3FE57'} title={'Confirmar'} onPress={() => { willUpdateFeedback('name', nameInput) }} />
                 </View>
 
+            )}
+            {title === 'Contato' && !contactData.loading && (
+                <ScrollView>
+                    <View>
+                        <Text style={styles.label}>Contatos atuais</Text>
+                        {contactData.data !== null ? (
+
+                            contactData.data.map((item, index) => (
+
+                                <View key={index}>
+                                    <Text style={styles.labelN}>{item.number}</Text>
+                                    <Text style={styles.labelN}>{item.email}</Text>
+                                </View>
+                            ))
+                        ) : (
+                            <View>
+                                <Text style={styles.title}>Você não possui contatos ainda</Text>
+                            </View>
+                        )}
+                    </View>
+                    <Text style={styles.title}>Novo contato</Text>
+                    <View style={{ gap: 30 }}>
+                        <Input label={'Novo numero'} keyboardType={'numeric'} onChange={(value, la) => setNumberInput(value)} />
+                        <Input label={'Novo e-mail'} onChange={(value, la) => setEmailInput(value)} />
+                    </View>
+                    <PressableButton marginTop={'90px'} bgColor={'#D3FE57'} title={'Confirmar'} onPress={() => { willSaveContact() }} />
+                </ScrollView>
+            )}
+
+
+
+            {title === 'Alterar foto de perfil' && (
+                <View style={{ alignItems: 'center' }}>
+                    {accountData.client.client.picture === null && image === '' && (
+                        <View style={styles.iconProfile}>
+                            <Ionicons name='person-outline' size={60} color={"#A2A2A2"} />
+                        </View>
+                    )}
+                    {image !== '' && (
+                        <Image source={{ uri: image }} style={styles.img} />
+                    )}
+                    
+                    
+                     <PressableButton bgColor={'#D3FE57'} marginTop={'70px'} title={'Escolher Foto'} onPress={gallery} />
+                    <PressableButton bgColor={'#D3FE57'} title={'Confirmar'} onPress={sendImageFunc}/>
+
+                </View>
             )}
         </View>
     )
 }
-{/* <TextInput
-secureTextEntry={obscureText}
-keyboardType={'default'}
-onChangeText={handleChange('password')}
-onFocus={() => { setFieldTouched('password') }}
-onBlur={() => { setFieldTouched('password', '') }}
-value={values.password}
-color={"#fff"}
-maxLength={15}
-style={{ height: "100%", width: "85%" }}
-/> */}
