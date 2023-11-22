67407d1f4f220b76d7bc5cca2caf69a07fc940bf
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-22 11:21:34 -0300

    Fixing moneyvalue from screen transfer

--- screens/Transfer/index.jsx
+++ screens/Transfer/index.jsx
@@ -1,4 +1,4 @@
-import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Alert } from 'react-native'
 import defaultStyle from '../../src/defaultStyle/style'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -10,8 +10,11 @@ import styles from './styles';
 
 export default function Transfer() {
 
-    const [moneyValue, setMoneyValue] = useState('');
+    const [moneyValue, setMoneyValue] = useState('0');
+    const [keyValue, setKeyValue] = useState('');
     const [isValueScreen, setIsValueScreen] = useState(true)
+    const [isCNPJ, setIsCNPJ] = useState(false)
+    const [valid, setValid] = useState(false)
 
     const { accountData } = useSelector(state => {
         return state.userReducer;
@@ -21,16 +24,57 @@ export default function Transfer() {
 
     const validValueTransfer = () => {
         const newValue = extractNumericNumber(moneyValue)
-        console.log(balance - newValue)
+        if (newValue === 0) {
+            Alert.alert(
+                '',
+                'O valor não pode ser igual a 0',
+                [
+                    {
+                        text: 'OK',
+                        style: 'destructive',
+                        onPress: () => { },
+                    },
+                ]
+            );
+            return
+        }
+        setIsValueScreen(false)
+        console.log(newValue)
     }
 
     function extractNumericNumber(str) {
-        const valorSemSimbolo = str.replace(/[^\d,]/g, '');
+        const valueWithoutSimbol = str.replace(/[^\d,]/g, '');
 
-        const valorNumerico = parseFloat(valorSemSimbolo.replace(',', '.'));
+        const numericValue = parseFloat(valueWithoutSimbol.replace(',', '.'));
 
-        return valorNumerico;
+        return numericValue;
     }
+
+
+
+    function setValueCpfCnpj(text) {
+        console.log(text.length)
+        if (text.length > 13) {
+            console.log('true')
+            setIsCNPJ(true)
+        }else{
+            console.log('false')
+            setIsCNPJ(false)
+        }
+        
+        setKeyValue(text)
+
+        if (text.length === 14) {
+            setValid(true)
+        } else if (text.length === 18) {
+            setValid(true)
+        } else {
+            setValid(false)
+        }
+    }
+
+
+
 
     return (
         isValueScreen ? (
@@ -64,7 +108,30 @@ export default function Transfer() {
 
             </SafeAreaView>
         ) : (
-            <View></View>
+            <SafeAreaView style={defaultStyle.container}>
+                <View style={defaultStyle.logoArea}>
+                    <Image
+                        source={require('../../assets/img/logo.png')}
+                    />
+                </View>
+
+                <View style={{ marginTop: 30 }}>
+                    <Text style={defaultStyle.title} >Para qual conta deseja transferir? (CPF ou CNPJ)</Text>
+                </View>
+
+                <TextInputMask
+                    type={isCNPJ ? 'cnpj' : 'cpf'}
+                    style={styles.input}
+                    value={keyValue}
+                    onChangeText={(text) => setValueCpfCnpj(text)}
+                />
+                {valid && (
+                    <TouchableOpacity style={styles.button} onPress={() => { }}>
+                        <Ionicons name='arrow-forward-outline' size={30} />
+                    </TouchableOpacity>
+                )}
+
+            </SafeAreaView>
         )
 
     )
