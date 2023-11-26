b1d4aff09237eaa9754864f84c892785086e3f38
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-26 17:18:04 -0300

    Add the isReceiver variable and fixing the component

--- components/StatementRow/index.jsx
+++ components/StatementRow/index.jsx
@@ -3,20 +3,48 @@ import styles from './styles'
 import React from 'react'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import defaultSyle from '../../src/defaultStyle/style'
+import { useSelector } from 'react-redux'
 
 
-export default function StatementRow({ title }) {
+export default function StatementRow({ props }) {
+    const { accountData } = useSelector(state => {
+        return state.userReducer
+    })
+
+    const isReceiver = (accountData.account.id === props.account_receiver)
+    const value = parseFloat(props.value);
+    const data = new Date(props.date);
+    const day = data.getDate();
+    const month = data.toLocaleString('default', { month: 'short' });
+
+
 
     return (
         <View style={styles.container}>
-            <View style={styles.rowIcon}>
-                <MaterialCommunityIcons name='transfer-down' color={'#fff'} size={15} />
+            <View style={styles.mainContainer}>
+            <View style={[styles.rowIcon, isReceiver && styles.rowGreenIcon]}>
+                <MaterialCommunityIcons name={isReceiver ? 'transfer-down' : 'transfer-up'} color={!isReceiver ? '#fff' : '#1D7536'} size={20} />
             </View>
             <View>
-                <Text style={styles.label}>Transferência enviada</Text>
-                <Text style={styles.subtitle}>Arianne da Silva Barbosa</Text>
+                {isReceiver ? (
+                    <View>
+                        <Text style={styles.label}>Transferência recebida</Text>
+                        <Text style={styles.subtitle}>De {props.client_sender}</Text>
+                    </View>
 
-                <Text style={styles.subtitle}>R$ 10,00</Text>
+                ) : (
+                    <View>
+                        <Text style={styles.label}>Transferência enviada</Text>
+                        <Text style={styles.subtitle}>Para {props.client_receiver}</Text>
+                    </View>
+
+                )}
+                <Text style={styles.subtitle}>{value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</Text>
+            </View>
+            </View>
+
+            <View>
+                <Text style={styles.subtitle}>{day} {month}</Text>
             </View>
 
 
