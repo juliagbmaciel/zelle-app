a5cec969cf55a6f31075821009f4461155f38f46
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-30 11:17:17 -0300

    :lipstick: feat: styles to the new pages

---
+++ components/LoanRow/styles.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#232321',
+        gap: 10
+    },
+    label: {
+        color: '#ccc',
+        fontFamily: 'regular',
+        fontSize: 15
+    },
+    labelContent: {
+        fontFamily: 'regular',
+        color: '#fff',
+        fontSize: 20
+    }
+
+})
+
+export default styles
---
+++ screens/LoanView/styles.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    input: {
+        // backgroundColor: '#fff',
+        height: 50,
+        color: '#fff',
+        fontFamily: 'medium',
+        fontSize: 23,
+        marginTop: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(255, 255, 255, 0.61)'
+    },
+    button: {
+        backgroundColor: '#D3FE57',
+        width: 60,
+        height: 60,
+        borderRadius: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 30,
+        right: 30,
+    }
+})
+
+export default styles;
